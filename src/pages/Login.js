d5c3989962875542899d00e30f64d68d968d08a4
ngquangreact <nguyen.quang24799@gmail.com>
2024-01-13 17:19:53 +0700

    Build Single Product page UI part 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
                                         <Link to="/forgot-password">Forgot Password ?</Link>
                                         <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
                                             <button type="submit" className="button border-0">Login</button>
-                                            <Link to="/sign-up" className="button singup">SignUp</Link>
+                                            <Link to="/sign-up" className="button signup">SignUp</Link>
                                         </div>
                                     </div>
                                 </form>
